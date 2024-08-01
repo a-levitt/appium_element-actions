b16e31f1f0779e56a923a10351a2ef798b5ebe1e
a-levitt <artemis.levitt@gmail.com>
2024-08-01 14:18:46 +0200

    Scrolling action

--- src/test/java/alevitt/ElementActions.java
+++ src/test/java/alevitt/ElementActions.java
@@ -1,6 +1,10 @@
 package alevitt;
 
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 
 import java.net.MalformedURLException;
 
@@ -8,5 +12,16 @@ public class ElementActions {
     public static void main(String[] args) throws MalformedURLException {
         AppiumDriver driver = DriverInitialization.initializeDriver();
 
+        WebElement menuElementViews = driver.findElement(AppiumBy.accessibilityId("Views"));
+        menuElementViews.click();
+
+        WebElement scroll = driver.findElement(AppiumBy.id("android:id/list"));
+        driver.executeScript("mobile: swipeGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement) scroll).getId(),
+                "direction", "up",
+                "percent", 0.75));
+
+        WebElement viewsElementTextField = driver.findElement(AppiumBy.accessibilityId("TextFields"));
+        viewsElementTextField.click();
     }
 }
