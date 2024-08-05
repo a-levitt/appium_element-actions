22608ffcba82c2b6c557a796c27b7b118b9fc258
a-levitt <artemis.levitt@gmail.com>
2024-08-05 13:25:46 +0200

    Android Gestures, longClick implementation

---
+++ src/test/java/alevitt/AndroidGestures.java
@@ -0,0 +1,31 @@
+package alevitt;
+
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+import java.net.MalformedURLException;
+
+public class AndroidGestures {
+    public static void main(String[] args) throws MalformedURLException {
+        AppiumDriver driver = DriverInitialization.initializeDriver();
+
+        longClickGesture(driver);
+    }
+
+
+        public static void longClickGesture(AppiumDriver driver) {
+            driver.findElement(AppiumBy.accessibilityId("Views")).click();
+            driver.findElement(AppiumBy.accessibilityId("Drag and Drop")).click();
+
+            WebElement longTouchCircle = driver.findElement(AppiumBy.id("io.appium.android.apis:id/drag_dot_1"));
+
+            // mobile: touchAndHold
+            driver.executeScript("mobile: longClickGesture", ImmutableMap.of(
+                    "elementId", ((RemoteWebElement) longTouchCircle).getId(),
+                    "duration", 1500
+            ));
+        }
+}
